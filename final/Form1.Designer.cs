b48fa76c47a52a61a43aaf494712036105b8387b
jalpate151 <jalpate151@SDS-L-D9QLG63>
2025-01-24 11:10:06 -0500

    Add project files.

---
+++ final/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playerBox = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // playerBox
+            // 
+            this.playerBox.AutoSize = true;
+            this.playerBox.Location = new System.Drawing.Point(276, 26);
+            this.playerBox.Name = "playerBox";
+            this.playerBox.Size = new System.Drawing.Size(28, 16);
+            this.playerBox.TabIndex = 0;
+            this.playerBox.Text = ">>>";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 521);
+            this.Controls.Add(this.playerBox);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label playerBox;
+    }
+}
+
---
+++ final/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace final
+{
+    public partial class Form1 : Form
+    {
+
+
+        Random random = new Random();
+
+        Rectangle player = new Rectangle(10, 150, 10, 10);
+
+        Rectangle Dot = new Rectangle(75, 195, 15, 15);
+
+        int playerScore = 0;
+        int playerSpeed = 4;
+
+        bool wPressed = false;
+        bool sPressed = false;
+        bool aPressed = false;
+        bool dPressed = false;
+        
+        
+        
+
+
+       
+        Pen blackPen = new Pen(Color.Black, 5);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wPressed = true;
+                    break;
+                case Keys.S:
+                    sPressed = true;
+                    break ;
+                case Keys.A:
+                    aPressed = true;
+                    break;
+                case Keys.D:
+                    dPressed = true;
+                    break;
+             
+
+                       
+
+               
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wPressed = false;
+                        break;
+                case Keys.S:
+                    sPressed = false;
+                    break;
+                case Keys.A:
+                    aPressed = false;
+                    break;
+                case Keys.D:
+                    dPressed = false;
+                    break;
+                
+
+                  
+            }
+
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(blackPen, player);
+            e.Graphics.DrawRectangle(blackPen, Dot);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            playerBox.Text = $"player's Score is {playerScore}";
+
+
+            if (wPressed == true && player.Y > 0)
+            {
+                player.Y -= playerSpeed;
+            }
+
+            if (sPressed == true && player.Y < this.Height - 10)
+            {
+                player.Y += playerSpeed;
+
+            }
+            Refresh();
+
+            if (aPressed == true && player.X > 0)
+            {
+                player.X -= playerSpeed;
+            }
+
+            if (dPressed == true && player.X < this.Width - 10)
+            {
+                player.X += playerSpeed;
+            }
+
+
+            if (player.IntersectsWith(Dot))
+            {
+                int randomX = random.Next(25,400);
+                int randomY = random.Next(25, 400);
+
+                Dot = new Rectangle(randomX, randomY, 15, 15);
+
+
+                playerScore++;
+
+            }
+
+            Refresh();
+
+            if(playerScore == 20)
+            {
+                
+            }
+
+        }
+    }
+}
+
+
